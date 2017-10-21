5ebdfa0fbcdcbd6d8dad12ebda8882d38b59de4f
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-10-21 19:58:36 -0400

    setup the main java to link to the xml and setup the UI

--- Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/MainActivity.java
+++ Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/MainActivity.java
@@ -2,12 +2,40 @@ package coen390.nicholas.sss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    //--------------------------------------------Declaring variables----------------------------------------------
+    //-------for objects needed in the home page-------
+    TextView title = null;
+    TextView hello = null;
+    Button goText = null;
+    Button goVoice = null;
+
+    //--------to LOG mainActivity events-----------
+    protected static final String TAG = "MainActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"The onCreate() event");
+
+        setupUI();
     }
+
+    //---------------------------Function that links the objects to their xml definitions-----------------------------
+    protected void setupUI()
+    {
+        title = (TextView) findViewById(R.id.Title);
+        hello = (TextView) findViewById(R.id.helloText); //link button
+        goText = (Button) findViewById(R.id.getText);
+        goVoice = (Button) findViewById(R.id.getVoice);
+    }
+
 }
