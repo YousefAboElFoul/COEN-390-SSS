94eff41560ae6370334e483d06c3030ac41e804d
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-10-22 18:12:23 -0400

    Made the activity for the voice page

---
+++ Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/voicePage.java
@@ -0,0 +1,13 @@
+package coen390.nicholas.sss;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class voicePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_page);
+    }
+}
