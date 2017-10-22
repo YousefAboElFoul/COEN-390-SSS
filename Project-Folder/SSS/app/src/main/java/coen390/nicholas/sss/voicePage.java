e98279dbd30ef86de895196284e23d8ad75bc364
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-10-22 18:14:39 -0400

    voice page activity java file basic code

--- Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/voicePage.java
+++ Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/voicePage.java
@@ -2,12 +2,30 @@ package coen390.nicholas.sss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class voicePage extends AppCompatActivity {
+public class voicePage extends AppCompatActivity
+{
+    //--------------------------------------------Declaring variables----------------------------------------------
+    //-------for objects needed in the home page-------
+    TextView title = null;
+
+    //--------to LOG textPage events-----------
+    protected static final String TAG = "textActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_page);
+        Log.d(TAG,"The onCreate() event");
+
+        setupUI();
+    }
+
+    //---------------------------Function that links the objects to their xml definitions-----------------------------
+    protected void setupUI()
+    {
+        title = (TextView) findViewById(R.id.voiceTitle);
     }
 }
