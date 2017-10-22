ad92da63bff6cbae313eaef01deddbbfb55040d4
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-10-22 19:21:40 -0400

    adding the action menu to the voice page

--- Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/voicePage.java
+++ Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/voicePage.java
@@ -1,8 +1,11 @@
 package coen390.nicholas.sss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class voicePage extends AppCompatActivity
@@ -28,5 +31,29 @@ public class voicePage extends AppCompatActivity
     protected void setupUI()
     {
         title = (TextView) findViewById(R.id.voiceTitle);
+    }
+
+    //-----------------------------------Functions for when the user presses the items--------------------------------------
+    //------when the action button gets pressed--------
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    //-----------when items are selected----------------
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Log.d(TAG, "The onOptions event");
+        switch (item.getItemId())
+        {
+            //action to switch to settings gets pressed
+            case R.id.goSettings:
+                Intent startIntent = new Intent(voicePage.this, settings.class);
+                startActivity(startIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
