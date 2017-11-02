7eb7082ae00a0020d48be8a046089e455d519d51
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-01 20:41:52 -0400

    added the menu to the help and settings activities

--- Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/help.java
+++ Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/help.java
@@ -1,8 +1,11 @@
 package coen390.nicholas.sss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class help extends AppCompatActivity
@@ -29,4 +32,42 @@ public class help extends AppCompatActivity
     {
         title = (TextView) findViewById(R.id.textTitleH);
     }
+
+
+    //-----------------------------------Functions for when the user presses the items--------------------------------------
+    //------when the action button gets pressed--------
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    //-----------when items are selected----------------
+    //attempted to make an onClick in the menu, but it crashed the app...
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "The onOptions event");
+        Intent startIntent;
+        switch (item.getItemId()) {
+            //action to switch to the letter page
+            case R.id.goLetter:
+                startIntent = new Intent(help.this, letterPage.class);
+                startActivity(startIntent);
+                return true;
+            //action to switch to word page
+            case R.id.goWord:
+                startIntent = new Intent(help.this, wordPageActivity.class);
+                startActivity(startIntent);
+                return true;
+            //action to switch to help
+            case R.id.goHelp:
+                return true;
+            //action to switch to settings
+            case R.id.goSettings:
+                startIntent = new Intent(help.this, settings.class);
+                startActivity(startIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/settings.java
+++ Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/settings.java
@@ -1,8 +1,11 @@
 package coen390.nicholas.sss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class settings extends AppCompatActivity
@@ -28,5 +31,41 @@ public class settings extends AppCompatActivity
     protected void setupUI()
     {
         title = (TextView) findViewById(R.id.settingTitle);
+    }
+
+    //-----------------------------------Functions for when the user presses the items--------------------------------------
+    //------when the action button gets pressed--------
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    //-----------when items are selected----------------
+    //attempted to make an onClick in the menu, but it crashed the app...
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "The onOptions event");
+        Intent startIntent;
+        switch (item.getItemId()) {
+            //action to switch to the letter page
+            case R.id.goLetter:
+                startIntent = new Intent(settings.this, letterPage.class);
+                startActivity(startIntent);
+                return true;
+            //action to switch to word page
+            case R.id.goWord:
+                startIntent = new Intent(settings.this, wordPageActivity.class);
+                startActivity(startIntent);
+                return true;
+            //action to switch to help
+            case R.id.goHelp:
+                startIntent = new Intent(settings.this, help.class);
+                startActivity(startIntent);
+                return true;
+            //action to switch to settings
+            case R.id.goSettings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
