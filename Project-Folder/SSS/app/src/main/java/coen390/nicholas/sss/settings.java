c79ce9a4324d33cb978d67b9a8f3820c91421f38
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-22 02:43:29 -0500

    completely refactored settings code and implemented the translation on the settings page

--- Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/settings.java
+++ Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/settings.java
@@ -1,29 +1,35 @@
 package coen390.nicholas.sss;
 
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 public class settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     //--------------------------------------------Declaring variables----------------------------------------------
     //-------for objects needed in the home page-------
     TextView title = null;
+    TextView languageSelection = null;
+    TextView voiceSelection = null;
+    TextView bluetoothSelection = null;
     private Spinner spinner;
-    TextToSpeech speaking;
-    private static final String[]paths = {"English", "French", "Arabic","Kharma"};
+    Button btn;
+
+    //----------options for language-------------
+    private static final String[]paths = {"English", "French", "Cameron","Kharma"};
+
+    //------------track variables----------------
+    private int languageTrack = 1;
+    private boolean voice = true;
+
     //--------to LOG textPage events-----------
     protected static final String TAG = "settingsActivity";
 
@@ -35,16 +41,6 @@ public class settings extends AppCompatActivity implements AdapterView.OnItemSel
         setContentView(R.layout.activity_settings);
         Log.d(TAG,"The onCreate() event");
 
-        // set up spinner for language selection
-        spinner = (Spinner)findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(settings.this,
-                android.R.layout.simple_spinner_item,paths);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-
-
         setupUI();
     }
 
@@ -52,37 +48,102 @@ public class settings extends AppCompatActivity implements AdapterView.OnItemSel
     protected void setupUI()
     {
         title = (TextView) findViewById(R.id.settingTitle);
-        Button btn = (Button) findViewById(R.id.button_gotobluetooth);
+        languageSelection = (TextView) findViewById(R.id.language_selection);
+        voiceSelection = (TextView) findViewById(R.id.voice_output);
+        bluetoothSelection = (TextView) findViewById(R.id.bluetooth_calibration);
+        btn = (Button) findViewById(R.id.button_gotobluetooth);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startIntent = new Intent(settings.this, Bluetooth.class);
-                startActivity(startIntent);
-            }
-        });
+        setText();
+        setSpinner();
 
-        ToggleButton toggle = (ToggleButton) findViewById(R.id.togglebuttonVoiceOn);
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) { AudioManager amanager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
-                    amanager.setStreamMute(AudioManager.STREAM_SYSTEM, false); // The toggle is disabled
-                    //unmute audio
-                    Toast.makeText(getApplicationContext(),
-                            "Toggle on.", Toast.LENGTH_SHORT).show();
-                    AudioManager volumeControl = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-                    volumeControl.setStreamMute(AudioManager.STREAM_MUSIC, false);
-                    // The toggle is enabled
-                } else {
-
-                    Toast.makeText(getApplicationContext(),
-                            "Toggle off.", Toast.LENGTH_SHORT).show();
-                    AudioManager volumeControl = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-                    volumeControl.setStreamMute(AudioManager.STREAM_MUSIC, true);
-                }
-            }
-        });
     }
+
+    private void setText()
+    {
+        String titleText = "SETTINGS";
+        String languageText = "Language: ";
+        String voiceText = "Voice Output";
+        String bluetoothText = "Bluetooth Configuration";
+        String bluetoothButton = "Configure";
+
+        if (languageTrack == 2)
+        {
+            titleText = "PARAMÈTRES";
+            languageText = "Langue: ";
+            voiceText = "Translation avec Voix";
+            bluetoothText = "Connection Bluetooth";
+            bluetoothButton = "Configuration";
+        }
+
+        title.setText(titleText);
+        languageSelection.setText(languageText);
+        voiceSelection.setText(voiceText);
+        bluetoothSelection.setText(bluetoothText);
+        btn.setText(bluetoothButton);
+    }
+
+    //----------------------------------Setup spinner-------------------------------
+    private void setSpinner()
+    {
+        // set up spinner for language selection
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(settings.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    //-------------------------------------------for going to bluetooth------------------------------------------------
+    public void goBluetooth(View view)
+    {
+        Intent startIntent = new Intent(settings.this, Bluetooth.class);
+        startActivity(startIntent);
+    }
+
+    //---------------------------------------For when the user choses to turn off voice------------------------------------
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.checkBoxVoice:
+                voice = checked;
+                break;
+        }
+    }
+
+    public boolean getVoiceOption(){return voice;}
+
+    //-------------------------------------Selection for the language dropdown----------------------------------------------
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            //run code depending on language selection
+            case 0:
+                Toast.makeText(getApplicationContext(), "English selected.", Toast.LENGTH_SHORT).show();
+                languageTrack = 1;
+                setText();
+                break;
+            case 1:
+                Toast.makeText(getApplicationContext(), "French selected.", Toast.LENGTH_SHORT).show();
+                languageTrack = 2;
+                setText();
+                break;
+            case 2:
+                Toast.makeText(getApplicationContext(), "Aha Good One", Toast.LENGTH_SHORT).show();
+                break;
+            case 3:
+                Toast.makeText(getApplicationContext(), "Very Good One.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 
     //-----------------------------------Functions for when the user presses the items--------------------------------------
     public void goHelp(View view)
@@ -108,35 +169,4 @@ public class settings extends AppCompatActivity implements AdapterView.OnItemSel
         Intent startIntent = new Intent(settings.this, letterPage.class);
         startActivity(startIntent);
     }
-
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        switch (position) {
-            //run code depending on language selection
-            case 0:
-                Toast.makeText(getApplicationContext(),
-                        "English selected.", Toast.LENGTH_SHORT).show();// Whatever you want to happen when the first item gets selected
-
-
-                break;
-            case 1:
-                Toast.makeText(getApplicationContext(),
-                        "French selected.", Toast.LENGTH_SHORT).show();// Whatever you want to happen when the second item gets selected
-
-
-                break;
-            case 2:
-                Toast.makeText(getApplicationContext(),
-                        "Arabic selected.", Toast.LENGTH_SHORT).show();// Whatever you want to happen when the thrid item gets selected
-                break;
-            case 3:
-                Toast.makeText(getApplicationContext(),
-                        "Kharma selected.", Toast.LENGTH_SHORT).show();// Whatever you want to happen when the thrid item gets selected
-                break;
-        }}
-
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-
 }
