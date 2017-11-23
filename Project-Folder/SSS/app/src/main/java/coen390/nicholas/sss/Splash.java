6d2ca952b484c096716f64fa05672a5328a1d808
YousefAboElFoul <yousefaboelfoul@gmail.com>
2017-11-23 17:42:15 -0500

    Added the splash screen with random pic

--- Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/Splash.java
+++ Project-Folder/SSS/app/src/main/java/coen390/nicholas/sss/Splash.java
@@ -1,29 +1,43 @@
 package coen390.nicholas.sss;
 
+import android.animation.AnimatorInflater;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
-
+    private TextView tv;
+    private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation anima = AnimationUtils.loadAnimation(this, R.anim.transition);
+        tv.startAnimation(anima);
+        iv.startAnimation(anima);
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
-        });
-    }
+        }, 3500);
+    }}
 
-}
+
+
+
